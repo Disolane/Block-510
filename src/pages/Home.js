cd676bf3ae09664d82fb4ec96c9bca6d1deebac9
Disolane <Andrew545180904@mail.ru>
2025-03-30 21:11:43 +0700

    Деревья

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,20 @@ import image2 from "../img/FourthyBuilding.png";
 import image3 from "../img/SixthyBuilding.png";
 import image4 from "../img/FirstBuilding.png";
 import image5 from "../img/FifthyBuilding.png";
+
 import image6 from "../img/Tree 07-1.png";
 import image7 from "../img/Tree 05-3.png";
 import image10 from "../img/Tree 05-3.png";
 import image11 from "../img/Tree 06-1.png"
 import image12 from "../img/Tree 06-2.png"
+
+import image13 from "../img/Tree 03-1.png"
+import image14 from "../img/Tree 03-2.png"
+import image15 from "../img/Tree 03-3.png"
+import image16 from "../img/Tree 07-2.png"
+import image17 from "../img/Tree 07-3.png"
+import image18 from "../img/Tree 08-1.png"
+
 const Home=()=>{
 return(
 <div className="body">
@@ -24,6 +33,18 @@ return(
       <img src={image10} alt="" className="img10"/>
       <img src={image11} alt="" className="img11" />
       <img src={image12} alt="" className="img12" />
+
+      <img src={image13} alt="" className="img13" />
+      <img src={image14} alt="" className="img14" />
+      <img src={image15} alt="" className="img15" />
+      <img src={image6} alt="" className="img16" />
+      <img src={image16} alt="" className="img17" />
+      <img src={image18} alt="" className="img18" />
+      <img src={image11} alt="" className="img19" />
+      <img src={image13} alt="" className="img20" />
+      <img src={image14} alt="" className="img21" />
+
+
       </div>
 );
 }
